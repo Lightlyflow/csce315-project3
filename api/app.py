f2d3ad7890305d8af360b379db2a2ef9b2cd6c15
Lightlyflow <tongqu777@gmail.com>
2023-10-27 11:19:04 -0500

    Added main and more venv names

--- api/app.py
+++ api/app.py
@@ -8,3 +8,6 @@ app.register_blueprint(customer.blueprint, url_prefix='/')
 app.register_blueprint(auth.blueprint, url_prefix='/auth')
 app.register_blueprint(menuboard.blueprint, url_prefix='/menuboard')
 app.register_blueprint(employee.blueprint, url_prefix='/employee')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
