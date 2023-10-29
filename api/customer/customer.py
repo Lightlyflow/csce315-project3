c47ad3e8a69d67fc18c8199c7c7c313f30137bcf
Muscheid1 <muscheid@DESKTOP-L207PBV>
2023-10-29 02:07:05 -0500

    Buggy shopping cart

--- api/customer/customer.py
+++ api/customer/customer.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, url_for
 
 customerBlueprint = Blueprint("customer", __name__, template_folder="templates")
 
 
-@customerBlueprint.route("/")
+@customerBlueprint.route("/", methods=['GET', 'POST'])
 def home():
     return render_template("customer_home.html")
