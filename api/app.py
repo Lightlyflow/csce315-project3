1efe23d93b3f2a50a140fe9a1783c0c2ddf5798d
Michael <michaelorgunov@gmail.com>
2023-10-30 00:26:09 -0500

    Created the template and container self-checkout

--- api/app.py
+++ api/app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 
 from api import manager, customer, auth, menuboard, employee
 
-app = Flask(__name__, static_folder=None)
+app = Flask(__name__)
 app.register_blueprint(manager.blueprint, url_prefix='/manager')
 app.register_blueprint(customer.blueprint, url_prefix='/')
 app.register_blueprint(auth.blueprint, url_prefix='/auth')
 app.register_blueprint(menuboard.blueprint, url_prefix='/menuboard')
 app.register_blueprint(employee.blueprint, url_prefix='/employee')
+
+app.static_url_path = '/static'
+app.static_folder = 'static'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
