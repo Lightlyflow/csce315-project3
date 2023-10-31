1a3aba4c53d873e26190d744cae3a31252209d4d
Muscheid1 <muscheid@DESKTOP-L207PBV>
2023-10-31 05:00:05 -0500

    Add and remove items from cart

--- api/customer/static/js/customer_home.js
+++ api/customer/static/js/customer_home.js
@@ -7,3 +7,44 @@ function toggleHeight(element) {
         menuDropDown.classList.toggle("active");
     }
 }
+
+function saveItem(item) {
+    var savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+    savedItems.push(item);
+    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+}
+
+function populateCart() {
+    var savedItems = JSON.parse(localStorage.getItem("savedItems"));
+    var pageCartItems = document.getElementById("pageCartItems");
+
+    while (pageCartItems.firstChild) {
+        pageCartItems.removeChild(pageCartItems.firstChild);
+    }
+
+    if (savedItems && savedItems.length > 0) {
+        savedItems.forEach(function(item) {
+            var rowDiv = document.createElement("div");
+            rowDiv.className = "row";
+            rowDiv.textContent = item;
+            pageCartItems.appendChild(rowDiv);
+        });
+    }
+    else {
+        var rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        rowDiv.textContent = "Your cart is empty.";
+        pageCartItems.appendChild(rowDiv);
+    }
+}
+
+function emptyCart() {
+    var savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+    savedItems = "";
+    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+    populateCart();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    populateCart();
+});
