a139dbd1a18174ce33c1c5f083f2d78a517e7979
Lightlyflow <tongqu777@gmail.com>
2023-10-31 13:43:42 -0500

    Added more styling and bugfixed getInventory when None

--- api/manager/manager_helper.py
+++ api/manager/manager_helper.py
@@ -2,4 +2,5 @@ from api.db import manager_querier
 
 
 def getInventory():
-    return manager_querier.getInventory()
+    result = manager_querier.getInventory()
+    return result if result is not None else []
--- api/manager/static/js/inventory.js
+++ api/manager/static/js/inventory.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $('#inventoryTable').DataTable({
-        "scrollY": "80vh",
+        "scrollY": "75vh",
         "scrollCollapse": true,
+        select: true,
         order: [[0, 'asc']],
+        paging: false,
     });
     $('.dataTables_length').addClass('bs-select');
 });
