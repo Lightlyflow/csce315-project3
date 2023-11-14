364af023d15dcc697ffe59ffecd084b0e9476869
Michael <michaelorgunov@gmail.com>
2023-11-13 20:12:56 -0600

    Complete overhaul of cashier interface

--- api/employee/employee.py
+++ api/employee/employee.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint, render_template, url_for, request, jsonify
+from flask_login import current_user
 from .employee_helper import getMenuCategories, getToppingNames, placeOrder, getWeather, getMenuData
 employeeBlueprint = Blueprint("employee", __name__, template_folder="templates", static_folder = "static")
 
@@ -9,7 +10,7 @@ def home():
     menuQuery = getMenuData()
     menuCategories = getMenuCategories(menuQuery)
     #print(menuCategories)
-    menuItems = {category: [(item[0], item[2]) for item in menuQuery if item[1] == category] for category in menuCategories}
+    menuItems = {category: [(item[0], item[2], item[3]) for item in menuQuery if item[1] == category] for category in menuCategories}
 
     # Weather api work to get temp and conditions
     weather = getWeather()
@@ -19,14 +20,16 @@ def home():
     # Toppings
     toppingNames = getToppingNames()
     
-    return render_template("employee_home.html", menuCategories=menuCategories, menuItems=menuItems, toppingNames=toppingNames, temperature=temperature, conditions=conditions)
+    return render_template("employee_home.html", menuCategories=menuCategories, menuItems=menuItems, toppingNames=toppingNames, temperature=temperature, conditions=conditions, current_user=current_user)
 
 @employeeBlueprint.route("/post_endpoint", methods=['POST'])
 def receive_saved_items():
     data = request.get_json()
-    if 'savedItems' in data:
-        saved_items = data['savedItems']
-        placeOrder(saved_items)
+    if 'savedMenuItems' in data:
+        savedItems = data['savedMenuItems']
+        placeOrder(savedItems)
         return jsonify({'message': 'Data received successfully'})
     
     return jsonify({'error': 'Invalid format'})
+
+
--- api/employee/static/js/employee_home.js
+++ api/employee/static/js/employee_home.js
@@ -1,3 +1,37 @@
+class MenuItem {
+    constructor(name, iceLevel, sweetness, topping1, topping2, topping3, quantity) {
+        this._name = name;
+        this._iceLevel = iceLevel;
+        this._sweetness = sweetness;
+        this._topping1 = topping1;
+        this._topping2 = topping2;
+        this._topping3 = topping3;
+        this._quantity = quantity;
+    }
+
+    get name() {
+        return this._name;
+    }
+    get iceLevel() {
+        return this._iceLevel;
+    }
+    get sweetness() {
+        return this.sweetness;
+    }
+    get topping1() {
+        return this._topping1;
+    }
+    get topping2() {
+        return this._topping2;
+    }
+    get topping3() {
+        return this._topping3;
+    }
+    get quantity() {
+        return this._quantity;
+    }
+}
+
 function toggleHeight(element) {
     element.classList.toggle("active");
 
@@ -8,31 +42,106 @@ function toggleHeight(element) {
     }
 }
 
-function holdItem(item) {
-    localStorage.setItem("heldItem", JSON.stringify(item));
-}
-
 function saveItem() {
-    var savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
-    var heldItem = JSON.parse(localStorage.getItem("heldItem"));
-    savedItems.push(heldItem);
-    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+    //Ice Options
+    var iceOptions = document.getElementsByName("iceOptions");
+    for (var i = 0; i < iceOptions.length; i++) {
+        var currentOption = iceOptions[i];
+
+        if (currentOption.checked) {
+            var iceId = currentOption.getAttribute('id');
+            if (iceId == "regularIce") {
+                var iceLevel = "Regular";
+            }
+            else if (iceId == "lessIce") {
+                var iceLevel = "Less";
+            }
+            else {
+                var iceLevel = "No";
+            }
+        }
+    }
+
+    //Toppings
+    let toppingList = [];
+    let toppingBoxes = document.getElementsByClassName('topping-col');
+    for (let i = 0; i < toppingBoxes.length; i++) {
+        let childElements = toppingBoxes[i].getElementsByClassName('topping-col-child');
+        if (childElements[0].checked) {
+            toppingList.push(childElements[1].innerHTML);
+        }
+    }
+    let unpickedToppings = 3 - toppingList.length;
+    for (let i = 0; i < unpickedToppings; i++) {
+        toppingList.push("null");
+    }
+
+    //Item Name
+    let menuItemName = document.getElementById("customizationName").innerHTML;
+
+    //Sweetness Level
+    let sweetnessLevel = document.getElementById('sweetnessLevel').value;
+
+    //Quantity
+    let quantity = document.getElementById('quantityPicker').value;
+
+    //Local Storage List
+    var savedMenuItems = JSON.parse(localStorage.getItem("savedMenuItems")) || [];
+    var newItem = new MenuItem(menuItemName, iceLevel, sweetnessLevel, toppingList[0], toppingList[1], toppingList[2], quantity);
+    savedMenuItems.push(newItem);
+    localStorage.setItem("savedMenuItems", JSON.stringify(savedMenuItems));
 }
 
 function populateCart() {
-    var savedItems = JSON.parse(localStorage.getItem("savedItems"));
+    var savedMenuItems = JSON.parse(localStorage.getItem("savedMenuItems"));
     var pageCartItems = document.getElementById("pageCartItems");
 
     while (pageCartItems.firstChild) {
         pageCartItems.removeChild(pageCartItems.firstChild);
     }
 
-    if (savedItems && savedItems.length > 0) {
-        savedItems.forEach(function(item) {
+    if (savedMenuItems && savedMenuItems.length > 0) {
+        savedMenuItems.forEach(function(item) {
             var rowDiv = document.createElement("div");
             rowDiv.className = "row";
-            rowDiv.textContent = item;
             pageCartItems.appendChild(rowDiv);
+
+            var colDiv1 = document.createElement("div");
+            colDiv1.className = "col";
+            colDiv1.textContent = item._name;
+
+            var colDiv2 = document.createElement("div");
+            colDiv2.className = "col";
+            colDiv2.textContent = item._iceLevel + " Ice";
+
+            var colDiv3 = document.createElement("div");
+            colDiv3.className = "col";
+            colDiv3.textContent = item._sweetness;
+
+            var colDiv4 = document.createElement("div");
+            colDiv4.className = "col";
+            let toppingList = [];
+            toppingList.push(item._topping1);
+            toppingList.push(item._topping2);
+            toppingList.push(item._topping3);
+            for (let i = 0; i < 3; i++) {
+                if (toppingList[i] != "null") {
+                    let toppingRow = document.createElement("div");
+                    toppingRow.className = "row";
+                    toppingRow.textContent = toppingList[i];
+                    colDiv4.appendChild(toppingRow);
+                }
+            }
+
+            var colDiv5 = document.createElement("div");
+            colDiv5.className = "col";
+            colDiv5.textContent = item._quantity;
+
+            rowDiv.appendChild(colDiv1);
+            rowDiv.appendChild(colDiv2);
+            rowDiv.appendChild(colDiv3);
+            rowDiv.appendChild(colDiv4);
+            rowDiv.appendChild(colDiv5);
         });
     }
     else {
@@ -41,14 +150,11 @@ function populateCart() {
         rowDiv.textContent = "Your cart is empty.";
         pageCartItems.appendChild(rowDiv);
     }
-
-    resetCustomization(); //Resets modal
 }
 
 function emptyCart() {
-    var savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
-    savedItems = "";
-    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+    var savedMenuItems = "";
+    localStorage.setItem("savedMenuItems", JSON.stringify(savedMenuItems));
     populateCart();
 }
 
@@ -56,18 +162,8 @@ document.addEventListener("DOMContentLoaded", function() {
     populateCart();
 });
 
-function dropdownFill(val, id) {
-    var y = document.getElementById(id);
-    var aNode = y.innerHTML = val + ' <span class="caret"></span>'; // Append 
-}
-
+//Resets modal to default values
 function resetCustomization() {
-    //Resetting the customization menu after clicking add to cart
-    //Topping dropdowns, reset to empty
-    document.getElementById("topping1Dropdown").innerText = "Topping 1";
-    document.getElementById("topping2Dropdown").innerText = "Topping 2";
-    document.getElementById("topping3Dropdown").innerText = "Topping 3";
-
     //Radio buttons for ice reset to regular, the default
     var ele = document.getElementsByName("iceOptions");
     for(var i = 0; i < ele.length; i++)
@@ -75,14 +171,22 @@ function resetCustomization() {
     ele[0].checked = true;
 
     //Slider for sweetness level reset to default value 50
-    document.getElementById('sweetnessLevel').value = 50;
+    document.getElementById('sweetnessLevel').value = 100;
+
+    //Reset of topping buttons
+    var toppings = document.querySelectorAll('input[name=toppingOptions]:checked');
+    for (var i = 0; i < toppings.length; i++) {
+        toppings[i].checked = false;
+    }
+
+    document.getElementById("quantityPicker").value = 1;
 }
 
 function sendSavedItemsToServer() {
-    var savedItems = JSON.parse(localStorage.getItem("savedItems"));
+    var savedMenuItems = JSON.parse(localStorage.getItem("savedMenuItems"));
 
-    if (savedItems && savedItems.length > 0) {
-        var data = { savedItems: savedItems };
+    if (savedMenuItems && savedMenuItems.length > 0) {
+        var data = { savedMenuItems: savedMenuItems };
 
         fetch('/post_endpoint', {
             method: 'POST',
@@ -94,3 +198,40 @@ function sendSavedItemsToServer() {
         emptyCart();
     }
 }
+
+//Checks if 3 toppings are selected to limit topping boxes
+function stopCheckTopping(element) {
+    if (document.querySelectorAll('input[name=toppingOptions]:checked').length > 3) {
+        element.checked = false;
+    }
+}
+
+
+//Gets item name to populate customization modal
+function setItemName(name) {
+    document.getElementById("customizationName").innerHTML = name;
+}
+
+
+// create clock 
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12;
+
+    var timeString = (hours < 10 ? '0' : '') + hours + ':' +
+                      (minutes < 10 ? '0' : '') + minutes + ':' +
+                      (seconds < 10 ? '0' : '') + seconds + ' ' + ampm;
+
+    document.getElementById('clock').innerHTML = timeString;
+}
+
+// update the clock every second
+setInterval(updateClock, 1000);
+
+// initial call to set the clock immediately
+updateClock();
