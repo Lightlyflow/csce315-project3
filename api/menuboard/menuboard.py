eb5b12c4ac8025e4678f47565ba12467b9646d18
CalebLewis0917 <caleblewis0917@gmail.com>
2023-11-14 18:35:14 -0600

    added text and column scaling

--- api/menuboard/menuboard.py
+++ api/menuboard/menuboard.py
@@ -18,7 +18,16 @@ def home():
     print(menuCategories)
     menuItems = {category: [(item[0], item[2]) for item in menuQuery if item[1] == category] for category in menuCategories}
 
+    numCategories = len(menuCategories)
+    numItems = 0
+    for cat in menuItems:
+        numItems += len(cat)
+    numItems -= 15
+    numCols = (numCategories//3) + 1
+    categorySize = str(1600 // numCategories) + "%"
+    itemSize = str(5400 // numItems) + "%"
+
     # Toppings
     toppingNames = getToppingNames()
     
-    return render_template("menuboard.html", menuCategories=menuCategories, menuItems=menuItems, toppingNames=toppingNames)
+    return render_template("menuboard.html", menuCategories=menuCategories, menuItems=menuItems, toppingNames=toppingNames, numCols=numCols, categorySize=categorySize, itemSize=itemSize)
---
+++ api/menuboard/static/js/menuboard.js
@@ -0,0 +1,15 @@
+function scaleMenu(numC, catSize, iSize){
+    document.getElementById("board").style.columnCount = numC;
+    var categories = document.querySelectorAll('h2');
+    for (var i = 0; i < categories.length; i++) {
+        categories[i].style.fontSize = catSize;
+    }
+    var items = document.querySelectorAll('li');
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.fontSize = iSize;
+    }
+}
+
+function test(){
+    
+}
