a1d9c24f4c1663ccb8067ff1696790ebe53cc9bf
Lightlyflow <tongqu777@gmail.com>
2023-11-29 10:40:55 -0600

    added landing page, set as default too

--- api/customer/customer.py
+++ api/customer/customer.py
@@ -1,17 +1,22 @@
 from flask import Flask, Blueprint, render_template, url_for, request, jsonify
 from .customer_helper import getMenuCategories, getToppingNames, placeOrder, getWeather, getMenuData
 
+customerBlueprint = Blueprint("customer", __name__, template_folder="templates", static_folder="static")
 
-customerBlueprint = Blueprint("customer", __name__, template_folder="templates", static_folder = "static")
 
-
-@customerBlueprint.route("/", methods=['GET'])
+@customerBlueprint.route("/")
 def home():
+    return render_template("customer_landing.html")
+
+
+@customerBlueprint.route("/order", methods=['GET'])
+def order():
     # Menu items dynamic loading
     menuQuery = getMenuData()
     menuCategories = getMenuCategories(menuQuery)
-    #print(menuCategories)
-    menuItems = {category: [(item[0], item[2], item[3]) for item in menuQuery if item[1] == category] for category in menuCategories}
+
+    menuItems = {category: [(item[0], item[2], item[3]) for item in menuQuery if item[1] == category] for category in
+                 menuCategories}
 
     # Weather api work to get temp and conditions
     weather = getWeather()
@@ -20,8 +25,10 @@ def home():
 
     # Toppings
     toppingNames = getToppingNames()
-    
-    return render_template("customer_home.html", menuCategories=menuCategories, menuItems=menuItems, toppingNames=toppingNames, temperature=temperature, conditions=conditions)
+
+    return render_template("customer_home.html", menuCategories=menuCategories, menuItems=menuItems,
+                           toppingNames=toppingNames, temperature=temperature, conditions=conditions)
+
 
 @customerBlueprint.route("/post_endpoint", methods=['POST'])
 def receive_saved_items():
@@ -30,5 +37,5 @@ def receive_saved_items():
         savedItems = data['savedMenuItems']
         placeOrder(savedItems)
         return jsonify({'message': 'Data received successfully'})
-    
+
     return jsonify({'error': 'Invalid format'})
