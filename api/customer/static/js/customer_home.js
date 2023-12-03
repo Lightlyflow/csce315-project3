3ac9b1200e0dec256cf194e55412788465612340
Michael <michaelorgunov@gmail.com>
2023-12-03 16:35:35 -0600

    Redid logic for scaling and fixed scaling issues

--- api/customer/static/js/customer_home.js
+++ api/customer/static/js/customer_home.js
@@ -215,20 +215,8 @@ function setItemName(name) {
 }
 
 function scaleText(pixelIncrement){
-    let names = document.getElementsByClassName('menuItemName');
-    let prices = document.getElementsByClassName('menuItemPrice');
-    let calories = document.getElementsByClassName('menuItemCalories');
-    for (var i = 0; i < names.length; i++) {
-        style = window.getComputedStyle(names[i], null).getPropertyValue('font-size');
-        currentSize = parseFloat(style);
-        names[i].style.fontSize = (currentSize + pixelIncrement) + 'px';
-        style = window.getComputedStyle(prices[i], null).getPropertyValue('font-size');
-        currentSize = parseFloat(style);
-        prices[i].style.fontSize = (currentSize + pixelIncrement) + 'px';
-        style = window.getComputedStyle(calories[i], null).getPropertyValue('font-size');
-        currentSize = parseFloat(style);
-        calories[i].style.fontSize = (currentSize + pixelIncrement) + 'px';
-    }
+    currentSize = parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+    document.documentElement.style.fontSize = (currentSize + pixelIncrement) + 'px';
 }
 
 const btnMenu = document.querySelector('.btnMenu');
