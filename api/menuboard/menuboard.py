0ef7f873fd660e2522e84c6ababf801ce0d73f59
Lightlyflow <tongqu777@gmail.com>
2023-12-06 20:42:20 -0600

    updated a few menuboard docstrings

--- api/menuboard/menuboard.py
+++ api/menuboard/menuboard.py
@@ -1,18 +1,22 @@
 from flask import Flask, Blueprint, render_template
 from .menuboard_helper import getMenuCategories, getToppingNames, getMenuData
 
+menuboardBlueprint = Blueprint("menuboard", __name__, template_folder="templates", static_folder="static")
 
-menuboardBlueprint = Blueprint("menuboard", __name__, template_folder="templates", static_folder = "static")
 
 @menuboardBlueprint.route("/", methods=['GET'])
 def home():
-    """Stores menu item and category information before rendering the menuboard page."""
+    """
+    Stores menu item and category information before rendering the menuboard page.
+    :return: render of menuboard page
+    """
     # Menu items dynamic loading
     menuQuery = getMenuData()
     menuCategories = getMenuCategories()
-    
+
     print(menuCategories)
-    menuItems = {category: [(item[0], item[2]) for item in menuQuery if item[1] == category] for category in menuCategories}
+    menuItems = {category: [(item[0], item[2]) for item in menuQuery if item[1] == category] for category in
+                 menuCategories}
 
     numCategories = len(menuCategories)
     print(numCategories)
@@ -20,11 +24,13 @@ def home():
     for cat in menuItems:
         numItems += len(cat)
     numItems -= 15
-    numCols = (numCategories//2)
-    categorySize = str((450*numCols) // numCategories) + "%"
-    itemSize = str((1350*numCols) // numItems) + "%"
+    numCols = (numCategories // 2)
+    categorySize = str((450 * numCols) // numCategories) + "%"
+    itemSize = str((1350 * numCols) // numItems) + "%"
 
     # Toppings
     toppingNames = getToppingNames()
-    
-    return render_template("menuboard.html", menuCategories=menuCategories, numCategories=numCategories, menuItems=menuItems, toppingNames=toppingNames, numCols=numCols, categorySize=categorySize, itemSize=itemSize)
+
+    return render_template("menuboard.html", menuCategories=menuCategories, numCategories=numCategories,
+                           menuItems=menuItems, toppingNames=toppingNames, numCols=numCols, categorySize=categorySize,
+                           itemSize=itemSize)
--- api/menuboard/menuboard_helper.py
+++ api/menuboard/menuboard_helper.py
@@ -1,19 +1,31 @@
 from api.db import menuboard_querier
 
+
 def getMenuData():
-    """Returns all menu items from the database."""
+    """
+    Returns all in stock menu items from the database.
+    :return: List of menu items
+    """
     return menuboard_querier.getMenuItems()
 
-def getMenuCategories() -> list():
-    """Returns all menu category names from the database in order of priority."""
+
+def getMenuCategories() -> []:
+    """
+    Returns all menu category names from the database in order of priority.
+    :return: Flattened list of category names
+    """
     results = menuboard_querier.getCategoryNames()
     categoryNames = []
     for cat in results:
         categoryNames.append(cat[0])
     return categoryNames
 
-def getToppingNames() -> list():
-    """Returns all topping names from the database."""
+
+def getToppingNames() -> []:
+    """
+    Returns all topping names from the database.
+    :return: Flattened list of topping names
+    """
     results = menuboard_querier.getToppingNames()
     toppingNames = []
     for topping in results:
